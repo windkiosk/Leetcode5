8baaed1e5338f8c87bc529ac5efcff192bedecb7
Developer Titan <titanduandeveloper@gmail.com>
2022-11-04 05:59:49 -0700

    Add problem desp for EstimatePi

--- daily_coding/circle_area/EstimatePi.java
+++ daily_coding/circle_area/EstimatePi.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+// This problem was asked by Google.
+// The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+// Hint: The basic equation of a circle is x2 + y2 = r2.
 public class EstimatePi {
     public static void main(String[] args) {
         System.out.println("How to EstimatePi");
