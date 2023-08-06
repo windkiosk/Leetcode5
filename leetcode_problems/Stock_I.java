955234b88574ab481b5b05c21754182867757e35
windkiosk <titanduandeveloper@gmail.com>
2023-08-06 12:53:55 -0700

    Update comment.

--- leetcode_problems/Stock_I.java
+++ leetcode_problems/Stock_I.java
@@ -4,9 +4,8 @@ package leetcode_problems;
 public class Stock_I {
 
   public static void main(String[] args) {
-
-//    int[] p = new int[] {7, 1, 5, 3, 6, 4};
-    int[] p = new int[] {7,6,4,3,1};
+    int[] p = new int[] {7, 1, 5, 3, 6, 4};
+//    int[] p = new int[] {7,6,4,3,1};
     System.out.println(maxProfit(p));
   }
 
