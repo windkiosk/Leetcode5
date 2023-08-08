67574ef066cd130d8d94522f321981cc6b64f092
windkiosk <titanduandeveloper@gmail.com>
2023-08-07 21:06:42 -0700

    Solution of UglyNumber.

---
+++ leetcode_problems/UglyNumber.java
@@ -0,0 +1,18 @@
+package leetcode_problems;
+
+public class UglyNumber {
+
+  public static void main(String[] args) {
+    System.out.println(isUgly(36));
+  }
+
+  public static boolean isUgly(int n) {
+    while (n > 1) {
+      if (n % 2 == 0) n /= 2;
+      else if (n % 3 == 0) n /= 3;
+      else if (n % 5 == 0) n /= 5;
+      else return false;
+    }
+    return n == 1;
+  }
+}
