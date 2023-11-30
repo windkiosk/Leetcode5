0719875198f3b5e6e365577e30f7aa6d534fc446
windkiosk <titanduandeveloper@gmail.com>
2023-11-29 21:43:48 -0800

    Add UglyNumber2 problem.

--- leetcode_problems/UglyNumber2.java
+++ leetcode_problems/UglyNumber2.java
@@ -2,7 +2,7 @@ package leetcode_problems;
 
 // https://leetcode.com/problems/ugly-number-ii/
 public class UglyNumber2 {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println(nthUglyNumber(10));
   }
 
@@ -16,27 +16,22 @@ public class UglyNumber2 {
 
     ugly[0] = 1;
 
-    for(int i = 1; i < n; i++)
-    {
-      next_ugly_num = Math.min(next_multiple_of_2,
-          Math.min(next_multiple_of_3,
-              next_multiple_of_5));
+    for (int i = 1; i < n; i++) {
+      next_ugly_num =
+          Math.min(next_multiple_of_2, Math.min(next_multiple_of_3, next_multiple_of_5));
 
       ugly[i] = next_ugly_num;
-      if (next_ugly_num == next_multiple_of_2)
-      {
-        i2 = i2+1;
-        next_multiple_of_2 = ugly[i2]*2;
+      if (next_ugly_num == next_multiple_of_2) {
+        i2 = i2 + 1;
+        next_multiple_of_2 = ugly[i2] * 2;
       }
-      if (next_ugly_num == next_multiple_of_3)
-      {
-        i3 = i3+1;
-        next_multiple_of_3 = ugly[i3]*3;
+      if (next_ugly_num == next_multiple_of_3) {
+        i3 = i3 + 1;
+        next_multiple_of_3 = ugly[i3] * 3;
       }
-      if (next_ugly_num == next_multiple_of_5)
-      {
-        i5 = i5+1;
-        next_multiple_of_5 = ugly[i5]*5;
+      if (next_ugly_num == next_multiple_of_5) {
+        i5 = i5 + 1;
+        next_multiple_of_5 = ugly[i5] * 5;
       }
     }
 
