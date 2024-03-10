1553972e951cb24183b9592f851d673f00447e58
Developer Titan <titanduandeveloper@gmail.com>
2024-03-10 13:01:10 -0700

    Add two way DecodeWays

---
+++ leetcode_problems/DecodeWays.java
@@ -0,0 +1,45 @@
+package leetcode_problems;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DecodeWays {
+
+    public static void main(String[] args) {
+        DecodeWays decodeWays = new DecodeWays();
+        
+        System.out.println(decodeWays.numDecodingsBfs("12"));
+
+        System.out.println(decodeWays.numDecodingsBfs("226"));
+
+        System.out.println(decodeWays.numDecodingsBfs("06"));
+
+        System.out.println(decodeWays.numDecodingsBfs("111111"));
+    }
+
+    public int numDecodingsBfs(String s) {
+        Deque<Integer> startPos = new LinkedList<>();
+        startPos.add(0);
+        int len = s.length();
+        int ret = 0;
+        while(!startPos.isEmpty()) {
+            int start = startPos.poll();
+            if (start == len) {
+                ret ++;
+                continue;
+            } else {
+                for (int end = start + 1; end <= len && end - start < 3; end ++) {
+                    String numString = s.subSequence(start, end).toString();
+                    int v = Integer.valueOf(numString);
+                    if (v < 1 || v > 26) {
+                        break;
+                    } else {
+                        startPos.add(end);
+                    }
+                }
+            }
+        }
+
+        return ret;
+    }
+}
