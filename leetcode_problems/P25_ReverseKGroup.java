380e3a1bc84ec21547fe2f291109aa9c6b270798
titanduandeveloper <titanduandeveloper@gmail.com>
2024-12-13 08:58:14 -0800

    Add reverseKGroup problem.

---
+++ leetcode_problems/P25_ReverseKGroup.java
@@ -0,0 +1,55 @@
+package leetcode_problems;
+
+import data.ListNode;
+
+public class P25_ReverseKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        // dummyNode
+        ListNode ff = new ListNode(0, head);
+
+        ListNode lastTail = ff;
+        ListNode curr = ff;
+
+        while (curr != null) {
+            boolean hasK = true;
+            // move curr k times
+            for (int i = 0; i < k; i++) {
+                curr = curr.next;
+                if (curr == null) {
+                    hasK = false;
+                    break;
+                }
+            }
+
+            if (!hasK) {
+                break;
+            }
+
+            // remember the next node since need to cut the tie
+            ListNode next = curr.next;
+            curr.next = null;
+            // reverse the node from lastTail.next
+            ListNode newHead = reverseList(lastTail.next);
+            ListNode newTail = lastTail.next;
+            lastTail.next = newHead;
+            newTail.next = next;
+            lastTail = newTail;
+            curr = lastTail;
+        }
+
+        return ff.next;
+    }
+
+    // return the new head after reversing
+    private ListNode reverseList(ListNode head) {
+        ListNode last = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = last;
+            last = curr;
+            curr = next;
+        }
+        return last;
+    }
+}
