dc6b68049108f8a1205b44ca193de22dde1002e8
titanduandeveloper <titanduandeveloper@gmail.com>
2024-12-13 08:18:11 -0800

    Add removeNthFromEnd problem.

---
+++ leetcode_problems/P19_RemoveNFromEnd.java
@@ -0,0 +1,31 @@
+package leetcode_problems;
+
+import data.ListNode;
+
+public class P19_RemoveNFromEnd {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (n == 0) return head;
+
+        // create dummy head for case that n == list.length
+        ListNode ff = new ListNode(0, head);
+
+        ListNode curr = ff;
+        // move first arrow n times firstly;
+        for (int i = 0; i < n; i++) {
+            curr = curr.next;
+        }
+
+        ListNode second = ff;
+        // move second along with curr
+        while (curr.next != null) {
+            curr = curr.next;
+            second = second.next;
+        }
+
+        // remove the next node after second
+        second.next = second.next.next;
+
+        return ff.next;
+    }
+}
